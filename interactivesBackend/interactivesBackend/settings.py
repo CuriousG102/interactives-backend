d66f12152cb081e1a6433908f9ab137436eab1ed
Miles <mileshutson@utexas.edu>
2015-03-12 18:23:40 -0500

    Fix

--- interactivesBackend/interactivesBackend/settings.py
+++ interactivesBackend/interactivesBackend/settings.py
@@ -65,11 +65,11 @@ WSGI_APPLICATION = 'interactivesBackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgis', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'django_interactives',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': secrets.DB_USER,
-        'PASSWORD': secrets.DB_PASS,
+        # 'PASSWORD': secrets.DB_PASS,
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
