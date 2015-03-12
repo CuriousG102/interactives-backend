4cf0f6a4a4cce6ebb4cc48bb41d00fd7c91140de
Miles <mileshutson@utexas.edu>
2015-03-12 15:47:35 -0500

    fix

--- interactivesBackend/crimeAPI/scraper/CrimeReport/CrimeReport/pipelines.py
+++ interactivesBackend/crimeAPI/scraper/CrimeReport/CrimeReport/pipelines.py
@@ -4,6 +4,8 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from datetime import datetime
+
 from scrapy.contrib.djangoitem import DjangoItem
 from crimeAPI.items import Crime, Offense
 
@@ -11,23 +13,32 @@ import django
 django.setup()
 
 class CrimeItem(DjangoItem):
-	django_model = Crime
+    django_model = Crime
 
 class CrimeReportPipeline(object):
     def process_item(self, item, spider):
-        i = CrimeItem()
-        i['report_number']
-        i['report_time']
-        i['offense_time']
-        i['offenses']
-        i['offense_address']
-        i['offense_census_tract']
-        i['offense_district']
-        i['offense_area_command']
-        i['offense_investigator_assigned']
-        i['geocoded']
-        i['geocode_location']
+        try:
+            i = Crime.objects.filter(report_number__exact = item['report_number'])[0]
+        except IndexError:
+            i = CrimeItem()
+            i['report_number'] = item['report_number']
 
+            time_format = "%a, %b-%d-%Y %H:%M"
+
+            i['report_time'] = datetime.strptime(item['report_time'],
+                                                 time_format)
+            i['offense_time'] = datetime.strptime(item['offense_time'],
+                                                  time_format)
+            i['offenses'] = item['offenses']
+            i['offense_address'] = item['offense_address']
+            i['offense_census_tract'] = item['offense_census_tract']
+            i['offense_district'] = item['offense_district']
+            i['offense_area_command'] = item['offense_area_command']
+            i['offense_investigator_assigned'] = item['offense_investigator_assigned']
+            i.save()
+        
+        return item
+        
 
 
 
