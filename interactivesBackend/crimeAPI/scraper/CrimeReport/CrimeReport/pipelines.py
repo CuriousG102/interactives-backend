77f72022f668a4a38a544fccbd3166f184ba6ec8
Miles <mileshutson@utexas.edu>
2015-03-12 20:44:05 -0500

    scrape

--- interactivesBackend/crimeAPI/scraper/CrimeReport/CrimeReport/pipelines.py
+++ interactivesBackend/crimeAPI/scraper/CrimeReport/CrimeReport/pipelines.py
@@ -12,19 +12,15 @@ from crimeAPI.models import Crime, Offense
 import django
 django.setup()
 
-from scrapy import log
 
 class CrimeItem(DjangoItem):
     django_model = Crime
 
 class CrimeReportPipeline(object):
     def process_item(self, item, spider):
-        log.msg("what", levl=log.INFO)
         try:
-            log.msg("in try", levl=log.INFO)
             i = Crime.objects.filter(report_number__exact = item['report_number'])[0]
         except IndexError:
-            log.msg("in except", level=log.INFO)
             i = CrimeItem()
             i['report_number'] = item['report_number']
 
@@ -39,15 +35,12 @@ class CrimeReportPipeline(object):
             i['offense_district'] = item['offense_district']
             i['offense_area_command'] = item['offense_area_command']
             i['offense_investigator_assigned'] = item['offense_investigator_assigned']
-            i = i.save(commit=False)
+            i = i.save()
             for offense in item['offenses']:
                 try:
                     offenseToAdd = Offense.objects.filter(name__exact = offense)[0]
                 except IndexError:
                     offenseToAdd = Offense(name = offense)
                 i.offenses.add(offenseToAdd)
-
-            i.save()
-            log.msg(i, level=log.INFO)
 
         return item
