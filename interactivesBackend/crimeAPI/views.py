20ed3aa2844ccc8d2386f5c85144eab85c49efac
Miles <mileshutson@utexas.edu>
2015-03-12 21:00:09 -0500

    fix

--- interactivesBackend/crimeAPI/views.py
+++ interactivesBackend/crimeAPI/views.py
@@ -3,6 +3,12 @@ from django.shortcuts import render
 from models import Crime
 from serializers import CrimeSerializer
 
+
+from django.shortcuts import render
+
+from models import Map, Event, SubEvent
+from serializers import MapSerializer, EventSerializer, SubEventSerializer
+from rest_framework import generics
 # Create your views here.
 class CrimeDetail(generics.RetrieveAPIView):
 	queryset = Crime.objects.all()
