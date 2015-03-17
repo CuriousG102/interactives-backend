50ac81642a92ca53721cf09883d5dc559269c588
Miles <mileshutson@utexas.edu>
2015-03-17 04:41:11 -0500

    fix

--- interactivesBackend/crimeAPI/management/commands/geocodecrimes.py
+++ interactivesBackend/crimeAPI/management/commands/geocodecrimes.py
@@ -8,30 +8,30 @@ class Command(BaseCommand):
     help = 'Geocode as many crimes as possible, starting with the most recent non-geocoded and working backwards'
 
     def handle(self, *args, **options):
-    	crimes = Crime.objects.all().filter(geocoded=False).order_by('-offense_time')[:2500]
-    	
-    	for crime in crimes:
-    		geocoder = geopy.geocoders.GoogleV3(api_key = secrets.API_KEY)
-    		if time.clock() - startTime < 0.2: # rate limited to 5 per second
+        crimes = Crime.objects.all().filter(geocoded=False).order_by('-offense_time')[:2500]
+        
+        for crime in crimes:
+            geocoder = geopy.geocoders.GoogleV3(api_key = secrets.API_KEY)
+            if time.clock() - startTime < 0.2: # rate limited to 5 per second
             sleepTime = .2 - (time.clock() - startTime) # prevent giving time.sleep
                                                         # a negative number
             if sleepTime > 0:
                 time.sleep(sleepTime)
 
             try:
-            	query = crime.offense_address
-            	crimeLocInfo = geocoder.geocode(query = crimeQuery)
-            	startTime = time.clock()
-            	crime.geocode_location = GEOSGeometry('POINT(' + crimeLocInfo.latitude ' ' + crimeLocInfo.longitude + ')')
-            	crime.geocoded = True
-            	crime.save()
+                query = crime.offense_address
+                crimeLocInfo = geocoder.geocode(query = crimeQuery)
+                startTime = time.clock()
+                crime.geocode_location = GEOSGeometry('POINT(' + crimeLocInfo.latitude ' ' + crimeLocInfo.longitude + ')')
+                crime.geocoded = True
+                crime.save()
             except geopy.exc.GeocoderQueryError:
-            	pass
+                pass
             except geopy.exc.GeocoderQuotaExceeded:
-            	# out of queries
-            	break
+                # out of queries
+                break
             except geopy.exc.GeocoderTimedOut:
-            	pass
+                pass
             #except AttributeError:
-            #	pass
-    		
+            #   pass
+            
