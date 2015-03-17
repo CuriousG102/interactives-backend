8849dbe4d6403bab3b1faccc46acd2dbe8beff2d
Miles <mileshutson@utexas.edu>
2015-03-17 04:02:37 -0500

    fix

--- interactivesBackend/crimeAPI/views.py
+++ interactivesBackend/crimeAPI/views.py
@@ -13,48 +13,48 @@ from dateutil.parser import parse
 
 # Create your views here.
 class CrimeDetail(generics.RetrieveAPIView):
-	queryset = Crime.objects.all()
-	serializer_class = CrimeDetailSerializer
+    queryset = Crime.objects.all()
+    serializer_class = CrimeDetailSerializer
 
 class CrimeList(generics.ListAPIView):
-	def get_queryset(self):
-		MAX_ALLOWED_DAYS = datetime.timedelta(days=7)
+    def get_queryset(self):
+        MAX_ALLOWED_DAYS = datetime.timedelta(days=7)
 
-		queryset = Crime.objects.all()
-		offenseTimeRange = [self.request.QUERY_PARAMS.get('offenseStartRange', None),
-							self.request.QUERY_PARAMS.get('offenseEndRange', None)]
-		bbBottomLeftX = self.request.QUERY_PARAMS.get('bbBottomLeftX', None)
-		bbBottomLeftY = self.request.QUERY_PARAMS.get('bbBottomLeftY', None)
-		bbTopRightX = self.request.QUERY_PARAMS.get('bbTopRightX', None)
-		bbTopRightY = self.request.QUERY_PARAMS.get('bbTopRightY', None)
-		offense = self.request.QUERY_PARAMS.get('offense', None)
+        queryset = Crime.objects.all()
+        offenseTimeRange = [self.request.QUERY_PARAMS.get('offenseStartRange', None),
+                            self.request.QUERY_PARAMS.get('offenseEndRange', None)]
+        bbBottomLeftX = self.request.QUERY_PARAMS.get('bbBottomLeftX', None)
+        bbBottomLeftY = self.request.QUERY_PARAMS.get('bbBottomLeftY', None)
+        bbTopRightX = self.request.QUERY_PARAMS.get('bbTopRightX', None)
+        bbTopRightY = self.request.QUERY_PARAMS.get('bbTopRightY', None)
+        offense = self.request.QUERY_PARAMS.get('offense', None)
 
-		timezone = pytz.timezone('US/Central')
-		if offenseTimeRange[0]:
-			offenseTimeRange[0] = parse(offenseTimeRange[0])
-			queryset = queryset.filter(offense_time__gte=offenseTimeRange[0])
-		else:
-			offenseTimeRange[0] = datetime.datetime.today() - MAX_ALLOWED_DAYS
-			queryset = queryset.filter(offense_time__gte=timezone.localize(offenseTimeRange[0]))
+        timezone = pytz.timezone('US/Central')
+        if offenseTimeRange[0]:
+            offenseTimeRange[0] = parse(offenseTimeRange[0])
+            queryset = queryset.filter(offense_time__gte=offenseTimeRange[0])
+        else:
+            offenseTimeRange[0] = datetime.datetime.today() - MAX_ALLOWED_DAYS
+            queryset = queryset.filter(offense_time__gte=timezone.localize(offenseTimeRange[0]))
 
-		if offenseTimeRange[1]:
-			offenseTimeRange[1] = parse(offenseTimeRange[1])
-		if offenseTimeRange[1] and ((offenseTimeRange[1] - offenseTimeRange[0]) <= MAX_ALLOWED_DAYS):
-			queryset = queryset.filter(offense_time__lte=offenseTimeRange[1])
-		else:
-			queryset = queryset.filter(offense_time__lte=timezone.localize(offenseTimeRange[0] + MAX_ALLOWED_DAYS))
+        if offenseTimeRange[1]:
+            offenseTimeRange[1] = parse(offenseTimeRange[1])
+        if offenseTimeRange[1] and ((offenseTimeRange[1] - offenseTimeRange[0]) <= MAX_ALLOWED_DAYS):
+            queryset = queryset.filter(offense_time__lte=offenseTimeRange[1])
+        else:
+            queryset = queryset.filter(offense_time__lte=timezone.localize(offenseTimeRange[0] + MAX_ALLOWED_DAYS))
 
-		if bbTopRightY and bbTopRightX and bbBottomLeftY and bbBottomLeftX:
-			geom = Polygon.frombbox(bbBottomLeftX, bbBottomLeftY, 
-									bbTopRightX, bbTopRightY)
-			queryset = queryset.filter(geocode_location__within=geom)
+        if bbTopRightY and bbTopRightX and bbBottomLeftY and bbBottomLeftX:
+            geom = Polygon.frombbox(bbBottomLeftX, bbBottomLeftY, 
+                                    bbTopRightX, bbTopRightY)
+            queryset = queryset.filter(geocode_location__within=geom)
 
-		if offense:
-			queryset = queryset.filter(offenses__pk=offense)
+        if offense:
+            queryset = queryset.filter(offenses__pk=offense)
 
-		return queryset
+        return queryset
 
-	serializer_class = CrimeListSerializer
+    serializer_class = CrimeListSerializer
 
 class CrimeCount(APIView):
     """
@@ -67,31 +67,31 @@ class CrimeCount(APIView):
         
         MAX_ALLOWED_DAYS = datetime.timedelta(days=7)
 
-		queryset = Crime.objects.all()
-		offenseTimeRange = [self.request.QUERY_PARAMS.get('offenseStartRange', None),
-							self.request.QUERY_PARAMS.get('offenseEndRange', None)]
-		bbBottomLeftX = self.request.QUERY_PARAMS.get('bbBottomLeftX', None)
-		bbBottomLeftY = self.request.QUERY_PARAMS.get('bbBottomLeftY', None)
-		bbTopRightX = self.request.QUERY_PARAMS.get('bbTopRightX', None)
-		bbTopRightY = self.request.QUERY_PARAMS.get('bbTopRightY', None)
-		offense = self.request.QUERY_PARAMS.get('offense', None)
+        queryset = Crime.objects.all()
+        offenseTimeRange = [self.request.QUERY_PARAMS.get('offenseStartRange', None),
+                            self.request.QUERY_PARAMS.get('offenseEndRange', None)]
+        bbBottomLeftX = self.request.QUERY_PARAMS.get('bbBottomLeftX', None)
+        bbBottomLeftY = self.request.QUERY_PARAMS.get('bbBottomLeftY', None)
+        bbTopRightX = self.request.QUERY_PARAMS.get('bbTopRightX', None)
+        bbTopRightY = self.request.QUERY_PARAMS.get('bbTopRightY', None)
+        offense = self.request.QUERY_PARAMS.get('offense', None)
 
-		timezone = pytz.timezone('US/Central')
-		if offenseTimeRange[0]:
-			offenseTimeRange[0] = parse(offenseTimeRange[0])
-			queryset = queryset.filter(offense_time__gte=offenseTimeRange[0])
+        timezone = pytz.timezone('US/Central')
+        if offenseTimeRange[0]:
+            offenseTimeRange[0] = parse(offenseTimeRange[0])
+            queryset = queryset.filter(offense_time__gte=offenseTimeRange[0])
 
-		if offenseTimeRange[1]:
-			offenseTimeRange[1] = parse(offenseTimeRange[1])
-			queryset = queryset.filter(offense_time__lte=offenseTimeRange[1])
+        if offenseTimeRange[1]:
+            offenseTimeRange[1] = parse(offenseTimeRange[1])
+            queryset = queryset.filter(offense_time__lte=offenseTimeRange[1])
 
-		if bbTopRightY and bbTopRightX and bbBottomLeftY and bbBottomLeftX:
-			geom = Polygon.frombbox(bbBottomLeftX, bbBottomLeftY, 
-									bbTopRightX, bbTopRightY)
-			queryset = queryset.filter(geocode_location__within=geom)
+        if bbTopRightY and bbTopRightX and bbBottomLeftY and bbBottomLeftX:
+            geom = Polygon.frombbox(bbBottomLeftX, bbBottomLeftY, 
+                                    bbTopRightX, bbTopRightY)
+            queryset = queryset.filter(geocode_location__within=geom)
 
-		if offense:
-			queryset = queryset.filter(offenses__pk=offense)
+        if offense:
+            queryset = queryset.filter(offenses__pk=offense)
 
 
 
