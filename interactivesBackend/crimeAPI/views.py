561eacf47967b06912c231f90b7701aae34bf370
Miles <mileshutson@utexas.edu>
2015-03-17 03:58:22 -0500

    New crime count endpoint. Less restrictions on this one.

--- interactivesBackend/crimeAPI/views.py
+++ interactivesBackend/crimeAPI/views.py
@@ -4,7 +4,7 @@ from models import Crime
 from serializers import CrimeDetailSerializer, CrimeListSerializer
 
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, APIView
 from django.contrib.gis.geos import Polygon
 
 import pytz
@@ -34,15 +34,15 @@ class CrimeList(generics.ListAPIView):
 			offenseTimeRange[0] = parse(offenseTimeRange[0])
 			queryset = queryset.filter(offense_time__gte=offenseTimeRange[0])
 		else:
-			
-			queryset = queryset.filter(offense_time__gte=timezone.localize(datetime.datetime.today() - MAX_ALLOWED_DAYS))
+			offenseTimeRange[0] = datetime.datetime.today() - MAX_ALLOWED_DAYS
+			queryset = queryset.filter(offense_time__gte=timezone.localize(offenseTimeRange[0])
 
 		if offenseTimeRange[1]:
 			offenseTimeRange[1] = parse(offenseTimeRange[1])
 		if offenseTimeRange[1] and ((offenseTimeRange[1] - offenseTimeRange[0]) <= MAX_ALLOWED_DAYS):
 			queryset = queryset.filter(offense_time__lte=offenseTimeRange[1])
 		else:
-			queryset = queryset.filter(offense_time__lte=timezone.localize(datetime.datetime.today()))
+			queryset = queryset.filter(offense_time__lte=timezone.localize(offenseTimeRange[0] + MAX_ALLOWED_DAYS))
 
 		if bbTopRightY and bbTopRightX and bbBottomLeftY and bbBottomLeftX:
 			geom = Polygon.frombbox(bbBottomLeftX, bbBottomLeftY, 
@@ -56,4 +56,44 @@ class CrimeList(generics.ListAPIView):
 
 	serializer_class = CrimeListSerializer
 
+class CrimeCount(APIView):
+    """
+    A view that returns the count of active users.
+    """
+    renderer_classes = (JSONRenderer, )
 
+    def get(self, request, format=None):
+        queryset = Crime.objects.all()
+        
+        MAX_ALLOWED_DAYS = datetime.timedelta(days=7)
+
+		queryset = Crime.objects.all()
+		offenseTimeRange = [self.request.QUERY_PARAMS.get('offenseStartRange', None),
+							self.request.QUERY_PARAMS.get('offenseEndRange', None)]
+		bbBottomLeftX = self.request.QUERY_PARAMS.get('bbBottomLeftX', None)
+		bbBottomLeftY = self.request.QUERY_PARAMS.get('bbBottomLeftY', None)
+		bbTopRightX = self.request.QUERY_PARAMS.get('bbTopRightX', None)
+		bbTopRightY = self.request.QUERY_PARAMS.get('bbTopRightY', None)
+		offense = self.request.QUERY_PARAMS.get('offense', None)
+
+		timezone = pytz.timezone('US/Central')
+		if offenseTimeRange[0]:
+			offenseTimeRange[0] = parse(offenseTimeRange[0])
+			queryset = queryset.filter(offense_time__gte=offenseTimeRange[0])
+
+		if offenseTimeRange[1]:
+			offenseTimeRange[1] = parse(offenseTimeRange[1])
+			queryset = queryset.filter(offense_time__lte=offenseTimeRange[1])
+
+		if bbTopRightY and bbTopRightX and bbBottomLeftY and bbBottomLeftX:
+			geom = Polygon.frombbox(bbBottomLeftX, bbBottomLeftY, 
+									bbTopRightX, bbTopRightY)
+			queryset = queryset.filter(geocode_location__within=geom)
+
+		if offense:
+			queryset = queryset.filter(offenses__pk=offense)
+
+
+
+        content = {'number':queryset.count()}
+        return Response(content)
