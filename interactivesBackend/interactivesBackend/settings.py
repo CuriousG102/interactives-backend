9be0cbcbae99f6d1a55c43ef29d6568bda995651
Miles <mileshutson@utexas.edu>
2015-03-20 00:19:37 -0500

    Fix the static_root for deploy

--- interactivesBackend/interactivesBackend/settings.py
+++ interactivesBackend/interactivesBackend/settings.py
@@ -92,7 +92,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_ROOT = '/opt/interactivesBackend/static/'
+STATIC_ROOT = '/opt/interactives-backend/static/'
 
 STATIC_URL = '/static/'
 
