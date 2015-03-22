22fd33a65636a3b46a2bb31c46fc5dfde9fef8e6
Miles <mileshutson@utexas.edu>
2015-03-22 17:19:57 -0500

    JSONP Support for Elizabeth

--- interactivesBackend/interactivesBackend/settings.py
+++ interactivesBackend/interactivesBackend/settings.py
@@ -98,7 +98,7 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
-        'rest_framework_yaml.renderers.JSONPRenderer',
+        'rest_framework_jsonp.renderers.JSONPRenderer',
     )
 }
 
