24ea67a4cd021d18c274a147db62130e2e8f423d
Miles <mileshutson@utexas.edu>
2015-03-26 23:03:56 -0500

    fix to how we deal with middleware

--- interactivesBackend/interactivesBackend/settings.py
+++ interactivesBackend/interactivesBackend/settings.py
@@ -114,7 +114,13 @@ if not DEBUG:
     }
     MIDDLEWARE_CLASSES = (
     'django.middleware.cache.UpdateCacheMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.cache.FetchFromCacheMiddleware',
     )
     CACHE_MIDDLEWARE_KEY_PREFIX = 'interactives'
