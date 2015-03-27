e69f1cf5dd46ed8ec8be3b16f60fdd5d504815dc
Miles <mileshutson@utexas.edu>
2015-03-26 23:04:23 -0500

    fix to how we deal with middleware

--- interactivesBackend/interactivesBackend/settings.py
+++ interactivesBackend/interactivesBackend/settings.py
@@ -45,16 +45,6 @@ INSTALLED_APPS = (
     'django.contrib.gis',
 )
 
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
-
 ROOT_URLCONF = 'interactivesBackend.urls'
 
 WSGI_APPLICATION = 'interactivesBackend.wsgi.application'
