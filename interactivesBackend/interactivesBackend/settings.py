5ac285404b8b4ca86996ed07dbe5face5ec34745
Miles <mileshutson@utexas.edu>
2015-03-26 23:14:53 -0500

    cache everything for one hour for now

--- interactivesBackend/interactivesBackend/settings.py
+++ interactivesBackend/interactivesBackend/settings.py
@@ -114,7 +114,7 @@ if not DEBUG:
     'django.middleware.cache.FetchFromCacheMiddleware',
     )
     CACHE_MIDDLEWARE_KEY_PREFIX = 'interactives'
-    CACHE_MIDDLEWARE_SECONDS = 60 * 10
+    CACHE_MIDDLEWARE_SECONDS = 60 * 60
     CACHE_MIDDLEWARE_ALIAS = 'default'
 
 REST_FRAMEWORK = {
