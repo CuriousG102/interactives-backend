330d346e5b5856d49d96a24ab21fb0fe5ef082a4
Miles <mileshutson@utexas.edu>
2015-04-25 01:27:33 -0500

    Bug fix

--- interactivesBackend/crimeAPI/views.py
+++ interactivesBackend/crimeAPI/views.py
@@ -44,11 +44,12 @@ class CrimeList(generics.ListAPIView):
 
         if offenseTimeRange[0]:
             offenseTimeRange[0] = parse(offenseTimeRange[0])
+            queryset = queryset.filter(offense_time__gte=offenseTimeRange[0])
         # else:
         #     central = timezone('US/Central')
         #     offenseTimeRange[0] = central.localize(datetime.datetime.today() - MAX_ALLOWED_DAYS)
         
-        queryset = queryset.filter(offense_time__gte=offenseTimeRange[0])
+        # queryset = queryset.filter(offense_time__gte=offenseTimeRange[0])
 
         if offenseTimeRange[1]:
             offenseTimeRange[1] = parse(offenseTimeRange[1])
