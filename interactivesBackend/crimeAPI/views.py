ea2255ad438f81057e2198c6dbc8c8c74e9070ed
Miles <mileshutson@utexas.edu>
2015-04-28 22:21:29 -0500

    Add an api endpoint for getting a number of crimes on the typical criteria but incremented by a user specified number of hours. This makes the task of graphing crimes much easier for a frontend developer. I have concerns about performance right now. Incrementing by 24 hours for a month worth of values means 30 sql queries counting entries that meet this criteria. That being said, I'm really not sure how to do this more efficiently, and counts appear to be very cheap in postgresql

--- interactivesBackend/crimeAPI/views.py
+++ interactivesBackend/crimeAPI/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from models import Crime, Offense, Category
 from serializers import CrimeDetailSerializer, CrimeListSerializer, OffenseListSerializer, CategoryListSerializer
 
+from django.http import Http404
 from django.db.models import Count
 from django.shortcuts import render
 from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
@@ -79,9 +80,61 @@ class CrimeList(generics.ListAPIView):
 
     serializer_class = CrimeListSerializer
 
+class CrimeCountIncrement(APIView):
+    """
+    A view that returns the count of crimes fulfilling the criteria given in the query
+    in increments specified by the query
+    """
+    renderer_classes = (JSONRenderer, BrowsableAPIRenderer, JSONPRenderer)
+
+    def get(self, request, format=None):
+        offenseTimeRange = [self.request.QUERY_PARAMS.get('offenseStartRange', None),
+                            self.request.QUERY_PARAMS.get('offenseEndRange', None)]
+        bbBottomLeftX = self.request.QUERY_PARAMS.get('bbBottomLeftX', None)
+        bbBottomLeftY = self.request.QUERY_PARAMS.get('bbBottomLeftY', None)
+        bbTopRightX = self.request.QUERY_PARAMS.get('bbTopRightX', None)
+        bbTopRightY = self.request.QUERY_PARAMS.get('bbTopRightY', None)
+        offense = self.request.QUERY_PARAMS.get('offense', None)
+        census = self.request.QUERY_PARAMS.get('census', None)
+        category = self.request.QUERY_PARAMS.get('category', None)
+        increment = self.request.QUERY_PARAMS.get('increment', None)
+
+        if (not increment) or (not offenseTimeRange[0]) or (not offenseTimeRange[1]):
+            return HttpResponseNotFound('<h1>You need an increment and times dummy</h1>')
+
+        offenseTimeRange[0] = parse(offenseTimeRange[0])
+        offenseTimeRange[1] = parse(offenseTimeRange[1])
+
+        # content = [{'number':queryset.count()}, ...]
+        content = []
+        increment = datetime.timedelta(hours=int(increment))
+        while (offenseTimeRange[0] + increment) <= offenseTimeRange[1]:
+            queryset = Crime.objects.all()
+            if bbTopRightY and bbTopRightX and bbBottomLeftY and bbBottomLeftX:
+                geom = Polygon.from_bbox((bbBottomLeftX, bbBottomLeftY, 
+                                        bbTopRightX, bbTopRightY))
+                queryset = queryset.filter(geocode_location__within=geom)
+
+            if offense:
+                queryset = queryset.filter(offenses__pk=offense)
+
+            if category:
+                queryset = queryset.filter(offenses__category__pk=category)
+
+            if census:
+                queryset = queryset.filter(offense_census_tract=census)
+            queryset = queryset.filter(offense_time__gte=offenseTimeRange[0])
+            queryset = queryset.filter(offense_time__lte=offenseTimeRange[0] + increment)
+            content.append({'number':queryset.count})
+
+        
+        return Response(content)
+
+
+
 class CrimeCount(APIView):
     """
-    A view that returns the count of active users.
+    A view that returns the count of crimes fulfilling the criteria given in the query.
     """
     renderer_classes = (JSONRenderer, BrowsableAPIRenderer, JSONPRenderer)
 
@@ -145,4 +198,3 @@ class CensusDistrictCrimeCount(APIView):
 
         content = queryset.values('offense_census_tract').annotate(count=Count('offense_census_tract'))
         return Response(content)
-
