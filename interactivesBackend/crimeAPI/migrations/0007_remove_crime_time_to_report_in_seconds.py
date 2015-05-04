84142f7b7fc516e74d3dad1403cfdbe628e4afe6
Miles Hutson <mileshutson@gmail.com>
2015-05-03 22:28:33 -0400

    Fix for my idiocy

---
+++ interactivesBackend/crimeAPI/migrations/0007_remove_crime_time_to_report_in_seconds.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('crimeAPI', '0006_crime_time_to_report_in_seconds'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='crime',
+            name='time_to_report_in_seconds',
+        ),
+    ]
