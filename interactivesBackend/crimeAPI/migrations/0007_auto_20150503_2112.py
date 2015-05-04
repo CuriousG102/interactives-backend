a015ed98ec1be984dc09586ec2394341973a03e4
Miles <mileshutson@utexas.edu>
2015-05-03 21:22:53 -0500

    fix typo

--- interactivesBackend/crimeAPI/migrations/0007_auto_20150503_2112.py
+++ interactivesBackend/crimeAPI/migrations/0007_auto_20150503_2112.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from django.db import models, migrations
 
 def subtract_times(apps, schema_editor):
-    Crime = apps.getmodel("crimeAPI", "Crime")
+    Crime = apps.get_model("crimeAPI", "Crime")
     for crime in Crime.objects.all():
         crime.time_to_report_in_seconds = (crime.report_time - crime.offense_time).total_seconds()
         crime.save()
