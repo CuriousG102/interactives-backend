e5454624896f5b9dee1e595214432a088081874b
Miles Hutson <mileshutson@gmail.com>
2015-05-03 22:29:22 -0400

    empty migration for data migration to new field

---
+++ interactivesBackend/crimeAPI/migrations/0008_auto_20150503_2129.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('crimeAPI', '0007_remove_crime_time_to_report_in_seconds'),
+    ]
+
+    operations = [
+    ]
