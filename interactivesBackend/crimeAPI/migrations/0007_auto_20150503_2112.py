2ac7e83dad31960b26b2f5b0954466d57392d046
Miles Hutson <mileshutson@gmail.com>
2015-05-03 22:12:49 -0400

    Empty migrations file taht will be used for a data migration for the time_to_report_in_seconds field

---
+++ interactivesBackend/crimeAPI/migrations/0007_auto_20150503_2112.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('crimeAPI', '0006_crime_time_to_report_in_seconds'),
+    ]
+
+    operations = [
+    ]
