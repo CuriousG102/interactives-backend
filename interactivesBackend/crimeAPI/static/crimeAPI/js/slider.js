a9058881620c06b7e84ab4799d28a0158f669e21
Miles <mileshutson@utexas.edu>
2015-05-09 23:27:07 -0500

    Make the slider start range match up with 'last week' category

--- interactivesBackend/crimeAPI/static/crimeAPI/js/slider.js
+++ interactivesBackend/crimeAPI/static/crimeAPI/js/slider.js
@@ -1,56 +1,16 @@
 var InteractiveController = {
     MILLISECONDS_IN_A_DAY:  24 * 60 * 60 * 1000,
     DAYS_IN_THE_PAST: 500,
-    DAYS_IN_PAST_FOR_DEFAULT: 7,
+    DAYS_IN_PAST_FOR_DEFAULT: 6,
     MAX_RANGE: 30,
     MIN_RANGE: 2,
-    DAYS_FROM_PRESENT: 7,
+    DAYS_FROM_PRESENT: 6,
     slider: null,
     clients: [],
 
     setup: function() {
-        // this.slider = $("#slider");
         var MILLISECONDS_IN_THE_PAST = this.DAYS_IN_THE_PAST * this.MILLISECONDS_IN_A_DAY;
         var MILLISECONDS_IN_THE_PAST_DEFAULT = this.DAYS_IN_PAST_FOR_DEFAULT * this.MILLISECONDS_IN_A_DAY;
-        // var MONTHS = ["Jan.", "Feb.", "March", "April",
-        //               "May", "June", "July", "Aug.", "Sep.",
-        //               "Oct.", "Nov.", "Dec."];
-        // this.slider.dateRangeSlider(
-        //     {
-        //         bounds: { // future improvement: account for timezone in setting bounds
-        //             min:  new Date(Date.now() - MILLISECONDS_IN_THE_PAST),
-        //             max: new Date()
-        //         },
-        //         defaultValues: {
-        //             min: new Date(Date.now() - MILLISECONDS_IN_THE_PAST_DEFAULT),
-        //             max: new Date()
-        //         },
-        //         step: {
-        //             days:1
-        //         },
-        //         range: {
-        //             min: {days: 2},
-        //             max: {days: 30}
-        //         },
-        //         formatter: function(val) {
-        //             var day = val.getDate(),
-        //                 month = val.getMonth(),
-        //                 year = val.getFullYear();
-        //             return MONTHS[month] + " " + day + ", " + year;
-        //         },
-        //         scales: [{
-        //             next: function(value) {
-        //                 var next = new Date(value);
-        //                 return new Date(next.setMonth(value.getMonth() + 1));
-        //             },
-        //             label: function(value) {
-        //                 return MONTHS[value.getMonth()] + " " 
-        //                 + value.getFullYear().toString().slice(-2);
-        //             }
-        //         }]
-        //     }
-        // );
-        // $("#updateButton").click(this.update.bind(this));
 
         $('#slider span').html(moment().subtract(this.DAYS_FROM_PRESENT + this.DAYS_IN_PAST_FOR_DEFAULT, 'days').format('MMMM D, YYYY') + ' - ' + moment().subtract(this.DAYS_FROM_PRESENT, 'days').format('MMMM D, YYYY'));
         this.slider = $("#slider").daterangepicker({
@@ -112,6 +72,9 @@ var InteractiveController = {
 
     addClient: function(clientFunction) {
         this.clients.push(clientFunction);
+        clientFunction(this.slider.data('daterangepicker').startDate.toDate(), 
+                       this.slider.data('daterangepicker').endDate.toDate(), 
+                       $('.catSelectorItem.active a').data('catID'));
     },
 
     update: function() {
